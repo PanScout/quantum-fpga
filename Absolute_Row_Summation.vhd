de5dab944565fc5297d823956981796d246c93d5
dav1nci-07 <kevinmarro2000@gmail.com>
2025-02-18 15:59:55 -0800

    abs value file to stdlogic

--- Absolute_Row_Summation.vhd
+++ Absolute_Row_Summation.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use IEEE.fixed_pkg.ALL;
+--use IEEE.fixed_pkg.ALL;
 use work.qTypes.ALL;
 
 -------------------------------------------------------------------------------
@@ -16,11 +16,14 @@ entity Absolute_Row_Summation is
   );
 end entity Absolute_Row_Summation;
 
-
 architecture Behavioral of Absolute_Row_Summation is
 
-  type partialSum_array is array (0 to numBasisStates-1, 0 to numBasisStates-1) of fixedHigh;
+  --type partialSum_array is array (0 to numBasisStates-1, 0 to numBasisStates-1) of fixedHigh;
+  constant DATA_WIDTH : natural := 64;  -- Adjust based on your actual needs
+  type partialSum_array is array (0 to numBasisStates-1, 0 to numBasisStates-1) 
+       of std_logic_vector(DATA_WIDTH-1 downto 0);
   signal partialSum : partialSum_array;
+
 
 begin
 
@@ -28,32 +31,44 @@ begin
   begin
 
     gen_cols: for j in 0 to numBasisStates-1 generate
+      signal abs_re : unsigned(DATA_WIDTH-1 downto 0);
+      signal abs_im : unsigned(DATA_WIDTH-1 downto 0);
     begin
 
       -- Handle first column (j=0) separately to avoid j-1 = -1
+      abs_re <= unsigned(abs(signed(A(i)(j).re)));
+      abs_im <= unsigned(abs(signed(A(i)(j).im)));
       gen_j0: if j = 0 generate
-        BS_j0: block
-          signal val : fixedHigh;
-        begin
-          val <= resize(abs(A(i)(j).re) + abs(A(i)(j).im), fixedHigh'high, fixedHigh'low);
-          partialSum(i, j) <= val;
-        end block BS_j0;
+        --BS_j0: block
+          --signal val : fixedHigh;
+        --begin
+          --val <= resize(abs(A(i)(j).re) + abs(A(i)(j).im), fixedHigh'high, fixedHigh'low);
+
+          --partialSum(i, j) <= val;
+        partialSum(i, j) <= std_logic_vector(abs_re + abs_im);
+        --end block BS_j0;
       end generate gen_j0;
 
       -- Handle columns j > 0
       gen_j_others: if j > 0 generate
-        BS_j: block
-          signal val : fixedHigh;
-        begin
-          val <= resize(abs(A(i)(j).re) + abs(A(i)(j).im), fixedHigh'high, fixedHigh'low);
-          partialSum(i, j) <= resize(partialSum(i, j-1) + val, fixedHigh'high, fixedHigh'low);
-        end block BS_j;
+        partialSum(i, j) <= std_logic_vector(
+          unsigned(partialSum(i, j-1)) + 
+          unsigned(abs_re) + 
+          unsigned(abs_im)
+        );
+        --BS_j: block
+          --signal val : fixedHigh;
+        --begin
+          --val <= resize(abs(A(i)(j).re) + abs(A(i)(j).im), fixedHigh'high, fixedHigh'low);
+          --partialSum(i, j) <= resize(partialSum(i, j-1) + val, fixedHigh'high, fixedHigh'low);
+        --end block BS_j;
       end generate gen_j_others;
 
     end generate gen_cols;
 
     rowSums(i).re <= partialSum(i, numBasisStates-1);
-    rowSums(i).im <= to_sfixed(0.0, fixedHigh'high, fixedHigh'low);
+    --rowSums(i).im <= to_sfixed(0.0, fixedHigh'high, fixedHigh'low);
+    rowSums(i).im <= (others => '0');
 
   end generate gen_rows;
 
