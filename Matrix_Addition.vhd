4bfc72600d471cdb3ed8bba9b70f4457ae41e6d7
Michael <mail@mwdenis.com>
2025-04-05 15:48:36 -0400

    yeaaaaaaaaaaaaaaaaaaaaaaaaah

--- Matrix_Addition.vhd
+++ Matrix_Addition.vhd
@@ -15,14 +15,14 @@ end Matrix_Addition;
 architecture Concurrent of Matrix_Addition is
 begin
     -- Generate adders for each matrix element
-    gen_row_adders : for row_idx in 0 to numBasisStates-1 generate
-        gen_col_adders : for col_idx in 0 to numBasisStates-1 generate
+    gen_row_adders : for row_idx in 0 to dimension-1 generate
+        gen_col_adders : for col_idx in 0 to dimension-1 generate
         begin
             -- Add real components
-            C(row_idx)(col_idx).re <= resize(A(row_idx)(col_idx).re + B(row_idx)(col_idx).re, fixed'high, fixed'low);
+            C(row_idx)(col_idx).re <= resize(A(row_idx)(col_idx).re + B(row_idx)(col_idx).re, fixed64'high, fixed64'low);
             
             -- Add imaginary components
-            C(row_idx)(col_idx).im <= resize(A(row_idx)(col_idx).im + B(row_idx)(col_idx).im, fixed'high, fixed'low);
+            C(row_idx)(col_idx).im <= resize(A(row_idx)(col_idx).im + B(row_idx)(col_idx).im, fixed64'high, fixed64'low);
         end generate gen_col_adders;
     end generate gen_row_adders;
 end architecture Concurrent;
